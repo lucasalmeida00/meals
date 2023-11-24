5bc7e297ad65a8031ced453b790e85dd796b3f1d
Lucas Almeida <lucasalmeidasilva0@gmail.com>
2023-11-24 14:25:20 -0300

    feat: adicionado rota para visualizar o detalhe da categoria

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,29 +1,49 @@
 import 'package:flutter/material.dart';
 import '../models/category.dart';
+import '../screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem({super.key, required this.category});
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => CategoriesMealsScreen(
+          category: category,
         ),
       ),
-      child: Center(
-        child: Text(
-          category.title,
-          style: Theme.of(context).textTheme.titleLarge,
+    );
+    // Navigator.of(context).pushNamed(
+    //   CategoriesMealsScreen.routeName,
+    //   arguments: category,
+    // );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Text(
+            category.title,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
         ),
       ),
     );
