4b9abb93056ed78d965914872a2d689c69edc660
Lucas Almeida <lucasalmeidasilva0@gmail.com>
2023-11-23 23:25:07 -0300

    feat: initial commit

---
+++ lib/components/category_item.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import '../models/category.dart';
+
+class CategoryItem extends StatelessWidget {
+  final Category category;
+
+  const CategoryItem({super.key, required this.category});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.5),
+            category.color,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(child: Text(category.title)),
+    );
+  }
+}
