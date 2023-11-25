b74ae17d67b3faa3ef8beb02ecf211a44eb264a3
Lucas Almeida <lucasalmeidasilva0@gmail.com>
2023-11-25 12:21:31 -0300

    feat: criado modelo de screen

---
+++ lib/models/screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Screen {
+  final String title;
+  final Widget screen;
+
+  const Screen({
+    required this.title,
+    required this.screen,
+  });
+}
